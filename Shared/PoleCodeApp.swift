635cffcf8bd1f3020e4666839da5c1ff3446e5ca
Niclas Eriksson <hexadecimal@me.com>
2021-12-30 14:09:26 +0100

    Initial Commit

---
+++ Shared/PoleCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoleCodeApp.swift
+//  Shared
+//
+//  Created by Niclas Eriksson on 2021-12-30.
+//
+
+import SwiftUI
+
+@main
+struct PoleCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
